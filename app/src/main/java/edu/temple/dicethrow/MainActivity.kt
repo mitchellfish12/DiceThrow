cf938a2c9a24452f761f38285010e519c06fca95
Mitchell Fishman <tuq42483@temple.edu>
2024-10-24 15:15:53 -0400

    Final commit

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.temple.dicethrow
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
